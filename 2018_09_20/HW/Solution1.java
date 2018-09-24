5bd86b3f00ca3d0c4ef205f9752344fc68b085ca
alstn2468 <alstn2468_@naver.com>
2018-09-25 00:06:43 +0900

    2018-09-25

--- 2018_09_20/HW/Solution1.java
+++ 2018_09_20/HW/Solution1.java
@@ -1,7 +1,63 @@
+import java.util.Scanner;
+
 public class Solution1 {
 
-    public static void main(String[] args) {
-        // 2 - 5
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("정수 3개를 입력하시오 >> ");
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+
+		scanner.close();
+
+		int max;
+
+		if (a < b) {
+
+			max = b;
+
+			if (max < c) {
+				max = c;
+			}
+
+		} else {
+
+			max = a;
+
+			if (max < c) {
+				max = c;
+			}
+
+		}
+
+		if (a == max) {
+
+			if (b + c > max) {
+				System.out.println("삼각형이 됩니다.");
+			} else {
+				System.out.println("삼각형이 안됩니다.");
+			}
+
+		} else if (b == max) {
+
+			if (a + c > max) {
+				System.out.println("삼각형이 됩니다.");
+			} else {
+				System.out.println("삼각형이 안됩니다.");
+			}
+
+		} else if (c == max) {
+
+			if (a + b > max) {
+				System.out.println("삼각형이 됩니다.");
+			} else {
+				System.out.println("삼각형이 안됩니다.");
+			}
+
+		}
+	}
 
 }
--- 2018_09_20/HW/Solution2.java
+++ 2018_09_20/HW/Solution2.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
+
 public class Solution2 {
 
-    public static void main(String[] args) {
-        // 2 - 6
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("1~99사이의 정수를 입력하시오 >> ");
+
+		int n = scanner.nextInt();
+
+		scanner.close();
+
+		if (n > 10) {
+
+			int tenDigit = n / 10;
+			int unitDigit = n % 10;
+
+			if (tenDigit == 3 || tenDigit == 6 || tenDigit == 9) {
+
+				if (unitDigit == 3 || unitDigit == 6 ||unitDigit == 9) {
+					System.out.println("박수짝짝");
+				} else {
+					System.out.println("박수짝");
+				}
+
+			} else {
+
+				if (unitDigit == 3 || unitDigit == 6 || unitDigit == 9) {
+					System.out.println("박수짝");
+				}
+
+			}
+
+		} else {
+
+			if (n == 3 || n == 6 || n == 9) {
+				System.out.println("박수짝");
+			}
+
+		}
+	}
 
 }
