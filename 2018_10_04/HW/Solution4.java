963b5214f24f2d3e788c3d3ed30e87b633f2cbdd
alstn2468 <alstn2468_@naver.com>
2018-10-07 19:10:27 +0900

    2018-10-07

--- 2018_10_04/HW/Solution4.java
+++ 2018_10_04/HW/Solution4.java
@@ -2,50 +2,55 @@
 import java.util.Scanner;
 
 public class Solution4 {
+	static String str[] = { "가위", "바위", "보" };
 
-	public static void game(String oper, int com) {
-		String str[] = { "가위", "바위", "보" };
+	public static void display(int result, String oper, int com) {
 
+		if (result == 0) {
+
+			System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
+								", 비겼습니다.");
+
+		} else if (result == 1) {
+
+			System.out.println("사용자 = " + oper +" , 컴퓨터 = " + str[com] +
+								", 사용자가 이겼습니다.");
+
+		} else if (result == 2) {
+
+			System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
+								", 사용자가 졌습니다.");
+
+		}
+	}
+
+	public static int game(String oper, int com) {
 		switch(com)
 		{
 		case 0 :
-			if (oper.equals("가위"))
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 비겼습니다.");
+			if (oper.equals("가위")) return 0;
 
-			else if (oper.equals("바위"))
-				System.out.println("사용자 = " + oper +" , 컴퓨터 = " + str[com] +
-									", 사용자가 이겼습니다.");
-			else
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 사용자가 졌습니다.");
-			break;
+			else if (oper.equals("바위")) return 1;
+
+			else return 2;
 
 		case 1 :
-			if (oper.equals("가위"))
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 사용자가 졌습니다.");
+			if (oper.equals("가위")) return 2;
 
-			else if (oper.equals("바위"))
-				System.out.println("사용자 = " + oper +" , 컴퓨터 = " + str[com] +
-									", 비겼습니다.");
-			else
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 사용자가 이겼습니다.");
-			break;
+
+			else if (oper.equals("바위")) return 0;
+
+			else return 1;
 
 		case 2 :
-			if (oper.equals("가위"))
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 사용자가 이겼습니다.");
+			if (oper.equals("가위")) return 1;
 
-			else if (oper.equals("바위"))
-				System.out.println("사용자 = " + oper +" , 컴퓨터 = " + str[com] +
-									", 사용자가 졌습니다.");
-			else
-				System.out.println("사용자 = " + oper + " , 컴퓨터 = " + str[com] +
-									", 비겼습니다.");
-			break;
+			else if (oper.equals("바위")) return 2;
+
+			else return 0;
+
+			default :
+				return -1;
 		}
 	}
 
@@ -64,7 +69,7 @@ public class Solution4 {
 
 			int com = (int)(Math.random() * 3);
 
-			game(oper, com);
+			display(game(oper, com), oper, com);
 
 		} while(true);
 
