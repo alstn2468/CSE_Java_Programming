70fa5246a237e33c0a41fee6b16dd8b111c53b5d
alstn2468 <alstn2468_@naver.com>
2018-10-13 04:33:09 +0900

    2018-10-13

--- 2018_10_11/HW/Solution3.java
+++ 2018_10_11/HW/Solution3.java
@@ -1,3 +1,20 @@
+/**
+ * 문제
+ * 다음 설명대로 Circle 클래스와 CircleManager 클래스를 환성하라.
+ * 다음 실행 결과와 같이 3개의 Circle 객체 배열을 만들고
+ * x, y, radius 값을 읽어 3개의 Circle 객체를 만들고
+ * show()를 이용하여 이들을 모두 출력하라.
+ *
+ * Input
+ * x, y, radius >> 3.0 3.0 5
+ * x, y, radius >> 2.5 2.7 6
+ * x, y, radius >> 5.0 2.0 4
+ *
+ * Output
+ * (3.0,3.0) 5
+ * (2.5,2.7) 6
+ * (5.0,2.0) 4
+ */
 
 import java.util.Scanner;
 
--- 2018_10_11/HW/Solution4.java
+++ 2018_10_11/HW/Solution4.java
@@ -1,8 +1,125 @@
+/**
+ * 문제
+ * 
+ *
+ * Input
+ *
+ * Output
+ *
+ */
+
+import java.util.Scanner;
+
+class Add {
+	private int a, b;
+
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	int calcuate() {
+		return this.a + this.b;
+	}
+}
+
+class Sub {
+	private int a, b;
+
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	int calcuate() {
+		return this.a - this.b;
+	}
+}
+
+class Mul {
+	private int a, b;
+
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	int calcuate() {
+		return this.a * this.b;
+	}
+}
+
+class Div {
+	private int a, b;
+
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	int calcuate() {
+		try {
+
+			return this.a / this.b;
+
+		} catch(ArithmeticException e) {
+
+			System.out.println("0으로 나눌 수 없습니다.");
+			return 0;
+
+		}
+	}
+}
 
 public class Solution4 {
 
 	public static void main(String[] args) {
-        4-11
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("두 정수와 연산자를 입력하시오 >> ");
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		char oper = scanner.next().charAt(0);
+
+		switch(oper)
+		{
+		case '+' :
+			Add add = new Add();
+
+			add.setValue(a, b);
+			System.out.println(add.calcuate());
+
+			break;
+
+		case '-' :
+			Sub sub = new Sub();
+
+			sub.setValue(a, b);
+			System.out.println(sub.calcuate());
+
+			break;
+
+		case '/' :
+			Div div = new Div();
+
+			div.setValue(a, b);
+			System.out.println(div.calcuate());
+
+			break;
+
+		case '*' :
+			Mul mul = new Mul();
+
+			mul.setValue(a, b);
+			System.out.println(mul.calcuate());
+
+			break;
+
+		default :
+			System.out.println("OPERATION INPUT ERROR!");
+		}
+
+		scanner.close();
 
 	}
 
