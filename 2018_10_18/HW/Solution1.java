a97f70c88a1e88445dd6a300662645760dea285e
alstn2468 <alstn2468_@naver.com>
2018-10-18 14:11:26 +0900

    Add HW Solution1.java & Solution2.java & Solution3.java

--- 2018_10_18/HW/Solution1.java
+++ 2018_10_18/HW/Solution1.java
@@ -1,8 +1,67 @@
+/**
+ * 문제
+ * Converter 클래스를 상속받아 Km를 mile(마일)로 변환하는
+ * Km2Mile 클래스를 작성하라. main() 메소드와 실행결과는 다음과 같다.
+ *
+ * Input
+ * Km을 입력하세요 >> 30
+ *
+ * Output
+ * 변환 결과 : 18.75mile입니다.
+ */
+
+import java.util.Scanner;
+
+abstract class Converter {
+	abstract protected double convert(double src);
+	abstract protected String getSrcString();
+	abstract protected String getDestString();
+
+	protected double ratio;
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println(getSrcString() + "을 " + getDestString() + "로 바꿉니다.");
+
+		System.out.print(getSrcString() + "을 입력하세요 >> ");
+		double val = scanner.nextDouble();
+		double res = convert(val);
+
+		System.out.println("변환 결과 : " + res + getDestString() + "입니다.");
+
+		scanner.close();
+	}
+}
+
+class Km2Mile extends Converter {
+	double mile;
+
+	public Km2Mile(double mile) {
+		this.mile = mile;
+	}
+
+	@Override
+	protected double convert(double src) {
+		return src / this.mile;
+	}
+
+	@Override
+	protected String getSrcString() {
+		return "km";
+	}
+
+	@Override
+	protected String getDestString() {
+		return "mile";
+	}
+}
 
 public class Solution1 {
 
     public static void main(String[] args) {
-        5-4
+    	Km2Mile toMile = new Km2Mile(1.6);
+    	toMile.run();
 
     }
 }
--- 2018_10_18/HW/Solution3.java
+++ 2018_10_18/HW/Solution3.java
@@ -1,8 +1,71 @@
+class Point {
+	public int x, y;
 
-public class Solution1 {
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	protected void move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class ColorPoint extends Point {
+	String color;
+
+	public ColorPoint() {
+		super();
+		this.color = "BLACK";
+	}
+
+	public ColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+
+	public void setXY(int x, int y) {
+		move(x, y);
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String toString() {
+		return this.color + "색의 " + "( " + this.x +
+				", " + this.y + " )의 점";
+
+	}
+}
+
+public class Solution3 {
 
     public static void main(String[] args) {
-        5-6
+    	ColorPoint zeroPoint = new ColorPoint();
+    	System.out.println(zeroPoint.toString() + "입니다.");
+
+    	ColorPoint cp = new ColorPoint(5, 5, "Yellow");
+    	cp.setXY(5, 5);
+    	cp.setColor("RED");
+
+    	String str = cp.toString();
+
+    	System.out.println(str + "입니다.");
 
     }
 }
